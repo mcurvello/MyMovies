99ea6133e94203cfd35e3da0bf5061cf55bed0e7
Marcio Curvello <marciocurvello@gmail.com>
2023-08-13 16:14:18 -0300

    add pin with annotations on map

--- MyMovies/MapViewController.swift
+++ MyMovies/MapViewController.swift
@@ -14,16 +14,54 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         searchBar.delegate = self
+        
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        requestAuthorization()
     }
     
+    func requestAuthorization() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestWhenInUseAuthorization()
+    }
 }
 
 extension MapViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
+        searchBar.resignFirstResponder()
+        
+        let request = MKLocalSearch.Request()
+        
+        request.naturalLanguageQuery = searchBar.text
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start { (response, error) in
+            if error == nil {
+                guard let response = response else {return}
+                
+                self.mapView.removeAnnotations(self.mapView.annotations)
+                
+                for item in response.mapItems {
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = item.placemark.coordinate
+                    annotation.title = item.name
+                    annotation.subtitle = item.url?.absoluteString
+                    
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        }
     }
 }
