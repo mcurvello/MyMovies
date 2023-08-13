1a5549944ed7839056104c95aaf2fd6a297b67c8
Marcio Curvello <marciocurvello@gmail.com>
2023-08-13 16:43:56 -0300

    add tracking routes

--- MyMovies/MapViewController.swift
+++ MyMovies/MapViewController.swift
@@ -24,6 +24,8 @@ class MapViewController: UIViewController {
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
         
+        mapView.delegate = self
+        
         requestAuthorization()
     }
     
@@ -61,6 +63,72 @@ extension MapViewController: UISearchBarDelegate {
                     
                     self.mapView.addAnnotation(annotation)
                 }
+            }
+        }
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        guard let annotation = view.annotation, let name = annotation.title else {return}
+        
+        let alert = UIAlertController(title: name, message: "O que deseja fazer?", preferredStyle: .actionSheet)
+        
+        if let subtitle = annotation.subtitle, let url = subtitle {
+            let urlAction = UIAlertAction(title: "Acessar site", style: .default) { (action) in
+                
+            }
+            alert.addAction(urlAction)
+        }
+        
+        let routeAction = UIAlertAction(title: "Traçar rota", style: .default) { (action) in
+            self.showRoute(to: annotation.coordinate)
+        }
+        alert.addAction(routeAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            
+            renderer.lineWidth = 8.0
+            renderer.strokeColor = .blue
+            
+            return renderer
+        } else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+    }
+    
+    func showRoute(to destination: CLLocationCoordinate2D) {
+        
+        let request = MKDirections.Request()
+        
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: mapView.userLocation.coordinate))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destination))
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { (response, error) in
+            if error == nil {
+                guard let response = response, let route = response.routes.first else {return}
+                
+                print("Nome:", route.name, "- distância:", route.distance, "- duração:", route.expectedTravelTime)
+                
+                for step in route.steps {
+                    print("Em", step.distance, "metros, ", step.instructions)
+                }
+                
+                self.mapView.removeOverlays(self.mapView.overlays)
+                
+                self.mapView.addOverlays([route.polyline], level: .aboveRoads)
             }
         }
     }
